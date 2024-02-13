3fa2855982b6dd449cdf75b74fdcb6e9a54daf38
Rajeev Reddy <rajeev@tectonic.so>
2024-02-13 23:07:25 +0530

    Added safe gaurding for cookie ops

--- src/mixpanel-persistence.js
+++ src/mixpanel-persistence.js
@@ -238,8 +238,14 @@ MixpanelPersistence.prototype.update_search_keyword = function (referrer) {
 // EXPORTED METHOD, we test this directly.
 MixpanelPersistence.prototype.update_referrer_info = function (referrer) {
     // If referrer doesn't exist, we want to note the fact that it was type-in traffic.
-    const finalReferrer = _.cookie.get('_tt_heimdall_referrer') || referrer;
-    _.cookie.remove('_tt_heimdall_referrer');
+    let routerReferrer = null;
+    try {
+        routerReferrer = _.cookie.get('_tt_heimdall_referrer');
+        _.cookie.remove('_tt_heimdall_referrer');
+    } catch (err) {
+        console.error('Error in getting router referrer, hence skipping');
+    }
+    const finalReferrer = routerReferrer || referrer;
     this.register_once({
         '$initial_referrer': finalReferrer || '$direct',
         '$initial_referring_domain': _.info.referringDomain(finalReferrer) || '$direct'
